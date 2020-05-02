163a4ca496472ccde09a5a8afeb6f8512d0a5f30
Weverton Icaro <weverton-icaro@users.noreply.github.com>
2020-05-02 00:04:12 -0300

    Prime Number
    Read an integer and say whether or not it is a prime number.

---
+++ Módulo02/Exercicios/exercicio52.py
@@ -0,0 +1,16 @@
+#Leia um número inteiro e diga se ele é ou não um número primo.
+
+num = int(input('Digite um número: '))
+tot = 0
+for c in range (1, num + 1):
+  if num % c == 0:
+    print('\033[33m', end='')
+    tot += 1
+  else :
+    print('\033[31m', end='')
+  print('{} '.format(c), end='')
+print('\n\033[mO número {} foi divisível {} vezes.'.format(num, tot))
+if tot == 2 :
+  print('Por isso ele é PRIMO!')
+else:
+  print('Por isso ele não é PRIMO!')
